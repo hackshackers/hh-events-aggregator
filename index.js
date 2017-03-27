31f9ed72bf9fab5d7eb80d8f7be17787e8e7e517
Josh Kadis <josh@alleyinteractive.com>
2017-03-27 17:39:16 -0400

    getting there

--- index.js
+++ index.js
@@ -15,34 +15,21 @@ const yellow = chalk.yellow;
 const config = require('./src/config');
 const parseGroupUrls = require('./src/parseGroupUrls');
 const processGroupUrls = require('./src/processGroupUrls');
-const S3Deploy = require('./src/S3Deploy');
 
 exports.init = () => {
-
-  // Set up stream to write output
-  const outputStream = fs.createWriteStream(config.outputPath);
-
-  // Upload to S3 when complete
-  outputStream.on('finish', () => {
-    console.log(green(`Finished writing to ${config.outputPath}`));
-    S3Deploy();
-  });
-
-  outputStream.write(config.vcalendar.prepend, 'utf8', () => {
-    // Start fetching
-    console.log(cyan(`Fetching groups data from ${config.APIUrl}`));
-    axios.get(config.APIUrl)
-      .then((response) => {
-        console.log(cyan('Fetched groups data, parsing URLs'));
-        return parseGroupUrls(response.data);
-      })
-      .catch((err) => {
-        console.log(red('Error fetching or parsing groups data'));
-        console.log(err);
-      })
-      .then((groupUrls) => {
-        console.log(cyan(`Found ${groupUrls.length} calendar URLs, parsing now...`));
-        processGroupUrls(groupUrls, outputStream);
-      });
-  });
+  // Start fetching
+  console.log(cyan(`Fetching groups data from ${config.APIUrl}`));
+  axios.get(config.APIUrl)
+    .then((response) => {
+      console.log(cyan('Fetched groups data, parsing URLs'));
+      return parseGroupUrls(response.data);
+    })
+    .catch((err) => {
+      console.log(red('Error fetching or parsing groups data'));
+      console.log(err);
+    })
+    .then((groupUrls) => {
+      console.log(cyan(`Found ${groupUrls.length} calendar URLs, parsing now...`));
+      processGroupUrls(groupUrls);
+    });
 }
