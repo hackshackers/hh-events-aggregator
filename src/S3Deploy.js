4c43d284308bc5096c95b39c18d5983aa256bb58
Josh Kadis <josh@alleyinteractive.com>
2017-03-27 10:02:43 -0400

    deploy to all s3 buckets

---
+++ src/S3Deploy.js
@@ -0,0 +1,27 @@
+const AWS = require('aws-sdk');
+const green = require('chalk').green;
+const fs = require('fs');
+const path = require('path');
+
+const config = require('./config');
+
+module.exports = function() {
+  function deployToBucket(Bucket, s3) {
+    s3.putObject({
+        Bucket,
+        Key: config.aws.key,
+        Body: fs.readFileSync(config.outputPath, 'utf8'),
+        ACL: 'public-read',
+        ContentType: 'text/calendar',
+    }, (err, data) => {
+      if (err) {
+        console.log(err, err.stack)
+      } else {
+        console.log(green(`Uploaded to ${Bucket}/${config.aws.key}`));
+      }
+    });
+  }
+
+  const s3 = new AWS.S3();
+  config.aws.buckets.forEach((bucket) => deployToBucket(bucket, s3));
+};
